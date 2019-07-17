435bb042a361e7d34cfe3e0349d5020672a705e1
hannaa0827 <53014067+hannaa0827@users.noreply.github.com>
2019-07-17 09:54:18 -0700

    Create csscode.cs

---
+++ csscode.cs
@@ -0,0 +1,28 @@
+#heading2{
+  font-size: 50px;
+  color: DarkMagenta ;
+  border: 5px solid dodgerblue;
+}
+
+.paragraphs {
+  font-size: 15px;
+  font-style: oblique;
+  color: MediumBlue;
+  border: 3px solid seashell;
+}
+
+img {
+  width: 200px;
+  height: 330px;
+  border: 7px solid white;
+}
+
+body{
+  background-color: pink;
+  text-align: center;
+}
+
+.headers{
+  font-size:30px;
+  color: rebeccapurple;
+}
